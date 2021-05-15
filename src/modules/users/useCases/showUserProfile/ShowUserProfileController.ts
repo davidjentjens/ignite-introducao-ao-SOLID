58b4631dd6d3a068db05bf545d59201dfd9ae69d
davidjentjens <davidjentjens@gmail.com>
2021-05-15 14:18:49 -0300

    Completar desafio

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const profile = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(201).json(profile);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
